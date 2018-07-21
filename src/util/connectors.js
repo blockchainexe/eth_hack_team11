b244e09e1205fb4bbe6491f7f5b8eabc96108ce1
hideUW <hideyukikomaki1996@keio.jp>
2018-07-21 10:31:57 +0900

    add connectors

--- src/util/connectors.js
+++ src/util/connectors.js
@@ -1,4 +1,7 @@
-import { Connect } from 'uport-connect'
+import { Connect, SimpleSigner } from 'uport-connect'
 
-export let uport = new Connect('TruffleBox')
-export const web3 = uport.getWeb3()
+export const uport = new Connect('ETH Hackathon App', {
+    clientId: '2oxpoRyQWcTam3wrhAqQ2LTigLa5yXjssWa',
+    network: 'rinkeby',
+    signer: SimpleSigner('9c6abeb3134606994bd42314ed431460baa605c5a366b45e090af21d2ca7e318')
+})
