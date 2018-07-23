2d7d4f712d220e9263dcd9c4a852e4180c4f17ee
hideUW <hideyukikomaki1996@keio.jp>
2018-07-23 21:39:38 +0900

    qr code

--- src/util/connectors.js
+++
@@ -1,7 +0,0 @@
-import { Connect, SimpleSigner } from 'uport-connect'
-
-export const uport = new Connect('ETH Hackathon App', {
-    clientId: '2oxpoRyQWcTam3wrhAqQ2LTigLa5yXjssWa',
-    network: 'rinkeby',
-    signer: SimpleSigner('9c6abeb3134606994bd42314ed431460baa605c5a366b45e090af21d2ca7e318')
-})
